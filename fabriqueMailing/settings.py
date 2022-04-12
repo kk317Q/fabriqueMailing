ed21c13c24bbf56fd4adc7e2124c52969502041b
Huseyn <webdev317@yandex.ru>
2022-04-13 02:12:22 +0300

    Have set up PostgreSQL

--- fabriqueMailing/settings.py
+++ fabriqueMailing/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mailingApp',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'fabriqueMailing.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'fabriqueMailingDataBase', 
+        'USER': 'postgres', 
+        'PASSWORD': '123456',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 
---
+++ mailingApp/urls.py
@@ -0,0 +1,7 @@
+#MailingAPP URLs are stated here, while link to them is defined in fabriqueMailing project URLs file
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
